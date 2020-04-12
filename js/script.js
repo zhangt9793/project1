9195544449df7bb03f9ec68a72ab9d4bcbcc6a25
Tianyi Zhang <zhangt9793@uwec.edu>
2020-04-12 19:41:39 +0800

    1

---
+++ js/script.js
@@ -0,0 +1,20 @@
+﻿function checkneirong() {
+    var name = document.getElementById("neirong").value;
+    if (name.trim() == "") {
+        alert("Please input the submission!");
+        document.form.neirong.focus();
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function checkForm() {
+    if (checkneirong()) {
+        alert("User submitted successfully!");
+        return true;
+    } else {
+        return false;
+    }
+}
+
